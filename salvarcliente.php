e631a3862e42a02172d3dc109aa78c52f2e3aa8b
José Olinda <joseolindad@yahoo.com.br>
2021-11-29 09:21:17 -0300

    Update salvarcliente.php

--- salvarcliente.php
+++ salvarcliente.php
@@ -3,4 +3,21 @@
 
 $nomeCliente = $_GET["nome"];
 
-echo $nomeCliente . "<br>";
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Salvar Cliente</title>
+</head>
+<body>
+    <table border="1">
+        <tr>
+            <td>Nome Completo</td>
+            <td><?php echo $nomeCliente; ?></td>
+        </tr>
+    </table>
+</body>
+</html>
